8a47b59c5a63335c010b542f1d286c91346e2c92
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2024-03-29 22:08:53 +0200

    sdsvdv

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const btn = document.querySelector(".btn");
 let currentPage = 1;
-function getImages(page = 1, perPage = 12) {
+function getImages(page = 1, perPage = 15) {
 
     const URL = `https://pixabay.com/api/?key=43137010-1be9baf787f0e3e744a248bbd&category=editors_choice&per_page=${perPage}&page=${page}`;
 
