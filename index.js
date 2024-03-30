7d05b66017514d5ed9075257115d43e5069491e7
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2024-03-30 10:29:01 +0200

    upupuupup

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const btn = document.querySelector(".btn");
+const btnFind = document.querySelector(".btn2");
+const input = document.querySelector(".input");
 let currentPage = 1;
-function getImages(page = 1, perPage = 15) {
 
-    const URL = `https://pixabay.com/api/?key=43137010-1be9baf787f0e3e744a248bbd&category=editors_choice&per_page=${perPage}&page=${page}`;
+
+function getImages(page = 1, perPage = 15) {
+    let inputf = input.value;
+    const key = "43137010-1be9baf787f0e3e744a248bbd";
+    const URL = `https://pixabay.com/api/?key=${key}&category=editors_choice&per_page=${perPage}&page=${page}&category=${inputf}`;
 
     fetch(URL)
     .then(response => response.json())
@@ -18,11 +23,16 @@ function getImages(page = 1, perPage = 15) {
     })
     .catch(error => console.error(error));
 }
-    getImages();
+    // getImages();
 
 btn.addEventListener("click", () => {
+    btn.textContent = "load more"
     console.log(currentPage);
     currentPage++;
     getImages(currentPage);   
      console.log(currentPage);
 });
+btnFind.addEventListener("click", () => {
+    btn.textContent = "load more"
+    getImages();
+})
